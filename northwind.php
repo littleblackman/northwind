6a064d1bd55e41f0ff9a5ca1445bf42a1adbf63c
littleBlackMan <littleblackman@users.noreply.github.com>
2019-04-19 10:55:59 +0200

    Update northwind.php
    déplacement de la logique métier / d'application en début de page
    utilisation de la syntaxe alternative
    rename des noms de la bdd

--- northwind.php
+++ northwind.php
@@ -1,35 +1,28 @@
-<!-- add the header in inc/ -->
-<?php
-include('inc/_header.php');
-include('inc/_function.php');
-
-$products = getProducts();
+<?php $products = getProducts();
 $orderDetails = getOrderDetails();
 $productOrders = getProductsOrders();
 $customers = getCustomerOne();
 
-//view the table
-//var_dump($orderDetails);
-//echo '<pre>';
-//print_r($productOrders);
-//echo '</pre>';
-?>
+////// REMARQUE : EN PROCEDURAL LA LOGIQUE METIER ET D'APPLICATION SONT PLACEES AU DEBUT DU CODE LE TOUT DANS UNE BALISE PHP
+;?>
+
+<!-- add the header in inc/ -->
+<?php include('inc/_header.php'); ?> <!--- DANS LA PARTIE VUE, ON OUVRE UNE BALISE PHP QU'ON REFERME SUR CHAQUE LIGNE
+<?PHP include('inc/_function.php');?>
+
 
 <header id='header'>
     <!-- table Customer / getCustomer() -->
-    <form method='POST' action='customer.php'>
-          <label for='customer'>Click to access at the customers</label>
-          <button type='submit' class='btn btn-primary btn-sm'>CUSTOMERS</button>
-    </form>
-    
+    <a href="customer.php" class="btn btn-primary btn-sm">CUSTOMERS</a> <!--- utiliser une balise a pour un lien + css pour le style button--->
+  
     <!-- list with products table Products / getProducts()-->
     <form method='POST' action='orderDetail.php'>
         <div id='divProduct'>
             <label for="idProduct">Select your product to have the orders</label>
             <select name='product' id='idProduct'>
-                <?php foreach($products as $keyProduct => $product) { ?>
+                <?php foreach($products as $keyProduct => $product): ?> <!--- DANS LA VUE UTILISEZ LA SYNTAXE ALTERNATIVE DE PHP --->
                     <option value='<?=$product["id"];?>' id='codeProduct'><?=$product['codeProduct'] . ' : ' . $product['nameProduct']?></option>
-                <?php } ?>  
+                <?php endforeach ?>  
             </select>
             <button type='submit' class='btn btn-success btn-sm'>ENVOYER</button>     
         </div>
@@ -39,9 +32,9 @@ $customers = getCustomerOne();
         <div id='divCustomer'>
             <label for='idCustomers'>Select the customer you want to consult</label>
             <select name='customers' id='idCustomers'>
-                <?php foreach($customers as $key => $customer) { ?>
+                <?php foreach($customers as $key => $customer): ?>
                     <option value='<?=$customer["idCustomer"];?>' id='codeCustomer'><?=$customer['companyName'] . ' : ' . $customer['firstName'] . ' ' .$customer['lastName'] ?></option>
-                <?php } ?>  
+                <?php endforeach ?>  
             </select>
             <button type='submit' class="btn btn-primary btn-sm">ENVOYER</button>
         </div>
@@ -65,11 +58,11 @@ $customers = getCustomerOne();
                 </tr>
                 <?php foreach($productOrders as $key => $productOrder):?>
                 <tr>
-                    <td id='id_key'><?=$productOrder['idProduct'];?></td>
-                    <td id='id_codeProduct'><?=$productOrder['codeProduct'];?></td>
-                    <td id='id_nameProduct'><?=$productOrder['nameProduct'];?></td>
-                    <td  id='id_quantity'><?=$productOrder['quantity'];?></td>
-                    <td id='id_unitPrice'><?=$productOrder['unitPrice'];?></td>
+                    <td id='id_key'><?=$productOrder['id'];?></td>
+                    <td id='code'><?=$productOrder['code'];?></td>
+                    <td id='name'><?=$productOrder['name'];?></td>
+                    <td  id='quantitu'><?=$productOrder['quantity'];?></td>
+                    <td id='price'><?=$productOrder['unitPrice'];?></td>
                     <td id='totalPrice'><?=$productOrder['quantity'] * $productOrder['unitPrice'];?></td>
                 </tr>
                 <?php endforeach; ?>
@@ -80,26 +73,26 @@ $customers = getCustomerOne();
         <article class='col-12 col-sm-6 col-lg-6'>
             <!-- table products -->
             <h3>PRODUCTS TABLE</h3>
-            <?php foreach($products as $keyProduct => $product) { ?>
+            <?php foreach($products as $keyProduct => $product) :?>
                    <ul>id Product : <?=$product['id'] ?>
-                        <li>code Product : <?=$product['codeProduct'] ?></li>
-                        <li>name Product :<?=$product['nameProduct']?></li>
+                        <li>code Product : <?=$product['code'] ?></li>
+                        <li>name Product :<?=$product['name']?></li>
                         <li>cost Product :<?=$product['costStandard']?></li>
                         <li>category     :<?=$product['category']?></li>
                     </ul>
-                <?php } ?>
+                <?php endforeach ?>
         </article>
         <article class='col-12 col-sm-6 col-lg-6'>
             <h3>ORDER_DETAILS TABLE</h3>    
             <!-- table order_details -->
-            <?php foreach($orderDetails as $keyOrderDetail => $orderDetail) { ?>
+            <?php foreach($orderDetails as $keyOrderDetail => $orderDetail) :?>
                    <ul>id Order Detail : <?=$orderDetail['id'] ?>
                         <li>id order   : <?=$orderDetail['idOrder'] ?></li>
                         <li>id Product :<?=$orderDetail['idProduct']?></li>
                         <li>quantity   :<?=$orderDetail['quantity']?></li>
                         <li>unit Price :<?=$orderDetail['unitPrice']?></li>
                     </ul>
-                <?php } ?>
+                <?php endforeach ?>
         </article>
     </div>    
 </div>
